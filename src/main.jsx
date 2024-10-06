c6dcd02c778f55340edb3d1fc91df047ef4f82b4
naimatofficial <naimat.official.10@gmail.com>
2024-10-06 10:20:52 +0500

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./App.css";
+import { Toaster } from "react-hot-toast";
+
+import { ThemeProvider } from "@material-tailwind/react";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
+createRoot(document.getElementById("root")).render(
+	<Provider store={store}>
+		<ThemeProvider>
+			<Toaster position="top-center" reverseOrder={false} />
+			<RouterProvider router={router} />
+		</ThemeProvider>
+	</Provider>
+);
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+export const BASE_URL = "http://127.0.0.1:5000"; // If using proxy
+
+export const USERS_URL = "/api/users";
+
+export const DEFAULT_IMG =
+	"https://www.proclinic-products.com/build/static/default-product.30484205.png";
