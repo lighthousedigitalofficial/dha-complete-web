51a45a1db78834a3054c15a27ccaf3d592a075e4
itsKiran55 <kiranraja557@gmail.com>
2024-10-07 12:14:41 +0500

    Created Facilites slice and Add integration

--- src/redux/slices/apiSlice.js
+++ src/redux/slices/apiSlice.js
@@ -24,7 +24,7 @@ export const apiSlice = createApi({
 		"AssociatesWebsite",
 		"Phases",
 		"Events",
-		"Facilies",
+		"Facilities",
 		"Affiliates",
 		"Banners",
 		"Notices",
--- src/redux/slices/facilities.js
+++ src/redux/slices/facilities.js
@@ -2,15 +2,51 @@ import { FACILITIES_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const facilitiesSlice = apiSlice.injectEndpoints({
-    endpoints: (builder)=> ({
-        getFacilities: builder.query({
-			query: () => ({
-				url:FACILITIES_URL,
-			}),
-			providesTags: ["Facilities"],
-			keepUnusedDataFor: 5,
-		}),
-    })
-})
+  endpoints: (builder) => ({
+    getFacilities: builder.query({
+      query: () => ({
+        url: FACILITIES_URL,
+      }),
+      providesTags: ["Facilities"],
+      keepUnusedDataFor: 5,
+    }),
 
-export const { useGetFacilitiesQuery } = facilitiesSlice; 
+    createFacilities: builder.mutation({
+      query: (facilities) => ({
+        url: FACILITIES_URL,
+        method: "POST",
+        body: facilities,
+      }),
+    }),
+
+    getFacility: builder.query({
+      query: (id) => ({
+        url: `${FACILITIES_URL}/${id}`,
+      }),
+    }),
+
+    updateFacilities: builder.mutation({
+      query: ({ id, ...data }) => ({
+        url: `${FACILITIES_URL}/${id}`,
+        method: "PUT",
+        body: data,
+      }),
+      keepUnusedDataFor: 5,
+    }),
+
+    deleteFacilities: builder.mutation({
+      query: (id) => ({
+        url: `${FACILITIES_URL}/${id}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateFacilitiesMutation,
+  useGetFacilitiesQuery,
+  useGetFacilityQuery,
+  useUpdateFacilitiesMutation,
+  useDeleteFacilitiesMutation,
+} = facilitiesSlice;
