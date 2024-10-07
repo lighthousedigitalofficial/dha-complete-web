037705089e7a02ebe5cc4058094be5c5e1210265
Faisal Khan <faisalkh01122@gmail.com>
2024-10-07 09:14:14 -0700

    deleted icon

--- src/redux/slices/importantNoticeSlice.js
+++ src/redux/slices/importantNoticeSlice.js
@@ -1,19 +1,4 @@
-// import { NOTICES_URL} from "../constants";
-// import { apiSlice } from "./apiSlice";
 
-// export const importantNoticeSlice = apiSlice.injectEndpoints({
-//     endpoints: (builder)=> ({
-//         getNotice: builder.query({
-// 			query: () => ({
-// 				url: NOTICES_URL,
-// 			}),
-// 			providesTags: ["Notice"],
-// 			keepUnusedDataFor: 5,
-// 		}),
-//     })
-// })
-
-// export const { useGetNoticeQuery } = importantNoticeSlice; 
 
 import { NOTICES_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
